c37ff28466ef1e213f819f4faaf1830b73d04740
Muhammad Saad <160862157+saadalee7@users.noreply.github.com>
2024-02-25 02:39:20 +0500

    Add files via upload

---
+++ 09-Favorite-Number/main.js
@@ -0,0 +1,4 @@
+"use strict";
+let favorite_number = 7;
+let message = "Here is your favorite number " + favorite_number;
+console.log(message);
---
+++ 09-Favorite-Number/main.ts
@@ -0,0 +1,3 @@
+let favorite_number :number = 7;
+let message :string = "Here is your favorite number " + favorite_number;
+console.log(message);
