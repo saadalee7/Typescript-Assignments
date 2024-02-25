6dfc0ff8161dbf1b8cc55c041c7c8266221781f2
Muhammad Saad <160862157+saadalee7@users.noreply.github.com>
2024-02-26 00:45:28 +0500

    Add files via upload

---
+++ 14-Guest-List/main.js
@@ -0,0 +1,6 @@
+"use strict";
+let guestLists = ['Imran Khan', 'Asif Zardari', 'Nawaz Sharif'];
+let i = 0;
+for (let i = 0; i < guestLists.length; i++) {
+    console.log('Dear Mr.' + guestLists[i] + ',\n\nYou are invited for a dinner.\n\nThank You!');
+}
---
+++ 14-Guest-List/main.ts
@@ -0,0 +1,5 @@
+let guestLists :string[] = ['Imran Khan' , 'Asif Zardari', 'Nawaz Sharif'];
+let i = 0;
+for(let i=0; i<guestLists.length; i++){
+    console.log('Dear Mr.' + guestLists[i] + ',\n\nYou are invited for a dinner.\n\nThank You!');
+}
