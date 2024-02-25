6c8dc0ff1e262409f478f267a79c5793969e3887
Muhammad Saad <160862157+saadalee7@users.noreply.github.com>
2024-02-25 10:13:14 +0500

    Add files via upload

---
+++ 12-Greetings/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let names = ["Ali", "Usman", "Umair", "Nabeel", "John"];
+let i = 0;
+const message = "Welcome to Typescript class ";
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ 12-Greetings/main.ts
@@ -0,0 +1,6 @@
+let names :string[] = ["Ali", "Usman", "Umair", "Nabeel", "John"];
+let i = 0;
+const message :string = "Welcome to Typescript class";
+for(let i=0; i < names.length; i++){
+    console.log(message + names[i]);
+}
