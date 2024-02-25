482617e30ef52f689466ff63630d00b63ec80de1
Muhammad Saad <160862157+saadalee7@users.noreply.github.com>
2024-02-25 10:09:58 +0500

    Add files via upload

---
+++ 11-Names/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Print Names using Array Method
+let names = ["Ali", "Usman", "Umair", "Nabeel", "Saad"];
+let i = 0;
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ 11-Names/main.ts
@@ -0,0 +1,8 @@
+// Print Names using Array Method
+
+let names :string[] = ["Ali", "Usman", "Umair", "Nabeel", "Saad"];
+let i = 0;
+
+for(let i = 0; i < names.length; i++){
+    console.log(names[0]);
+}
