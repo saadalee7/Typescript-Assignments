65e5d752a559f354f9b3836496771b39313daf83
Muhammad Saad <160862157+saadalee7@users.noreply.github.com>
2024-02-26 20:48:37 +0500

    Add files via upload

---
+++ 22-Intentional-Error/main.ts
@@ -0,0 +1,12 @@
+// If you haven’t received an array index error in one of your programs yet, try to make one happen.
+
+// creating array
+let newArry :string[] = ['Imran Khan', 'Nawaz Sharif', 'Asif Zardari'];
+
+// asseccing array by index
+console.log(newArry[3]);
+
+// Make sure you correct the error before closing the program
+
+// correction
+console.log(newArry);
