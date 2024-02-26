80460555b10810ef367c1ccc0e7e04c3ab66978d
Muhammad Saad <160862157+saadalee7@users.noreply.github.com>
2024-02-26 17:37:32 +0500

    Add files via upload

---
+++ 16-More-Guests/main.ts
@@ -0,0 +1,34 @@
+let guestLists :string[] = ['Imran khan', 'Nawaz Sharif', 'Asif Zardari'];
+let i = 0;
+for (let i = 0; i < guestLists.length; i++) {
+
+console.log('Dear Mr.' + guestLists[i] + ',\n\nIt is our plasure to invite you in our party.\n\nThank You!');
+
+}
+
+let absentGuest :string = 'Imran Khan';
+
+let newGuest :string = 'Kamran Tassori';
+
+guestLists[0] = newGuest;
+for (let i = 0; i < guestLists.length; i++) {
+
+    console.log('Dear Mr.' + guestLists[i] + ',\n\nIt is our plasure to invite you in our party.\n\nThank You!');
+
+}
+
+console.log(`Mr. ${absentGuest} is not comming to the party.`);
+
+console.log('Good News! We find Big Table so we are inviting 3 more Guests.');
+
+guestLists.unshift('Zia Khan');
+guestLists.splice(2, 0, 'Shehbaz Sharif');
+guestLists.push('Bilawal Zardari');
+
+// [Zia Khan, Kamran Tassori, Shebaz Sharif, Nawaz Sharif, Asif Zardari, Biwalal Zardari] 3 more guests added.
+
+for (let i = 0; i < guestLists.length; i++) {
+
+    console.log('Dear Mr.' + guestLists[i] + ',\n\nIt is our plasure to invite you in our party.\n\nThank You!');
+
+}
