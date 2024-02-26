a052934c6944dc16d5a7d8e2d0fe5b706a81a47c
Muhammad Saad <160862157+saadalee7@users.noreply.github.com>
2024-02-26 20:36:47 +0500

    Add files via upload

---
+++ 21-Think-of-Something-2/main.js
@@ -0,0 +1,19 @@
+"use strict";
+// They think of something you could store in a TypeScript Object
+// detail is object
+let detail = {
+    Name: 'Saad',
+    Age: 26,
+    Nationality: 'Pakistani',
+    Student: true
+};
+// printing detail
+console.log(detail);
+// car is object
+let car = {
+    Modle: 'Toyota',
+    Colour: 'Black',
+    Automatic: true
+};
+// printing car
+console.log(car);
