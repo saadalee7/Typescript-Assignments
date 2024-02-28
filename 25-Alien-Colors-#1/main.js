281cde7628cd30949025e8bde16e6495ae2a00cf
Muhammad Saad <160862157+saadalee7@users.noreply.github.com>
2024-02-28 17:16:13 +0500

    Add files via upload

---
+++ 25-Alien-Colors-#1/main.js
@@ -0,0 +1,11 @@
+"use strict";
+let alien_color = 'green';
+// Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+if (alien_color === 'green') {
+    console.log('Congratulations! You just earned 5 points.');
+}
+alien_color = 'red';
+// Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+if (alien_color === 'green') {
+    console.log('Congratulations! You just earned 5 points.');
+}
