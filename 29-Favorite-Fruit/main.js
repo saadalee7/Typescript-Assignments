7d60cbe6314e8f739eb8dcb0d02ea4cabea78144
Muhammad Saad <160862157+saadalee7@users.noreply.github.com>
2024-03-01 00:36:38 +0500

    Add files via upload

---
+++ 29-Favorite-Fruit/main.js
@@ -0,0 +1,17 @@
+"use strict";
+let favorite_fruits = ['Apple', 'Mango', 'Banana'];
+if (favorite_fruits.includes('Apple')) {
+    console.log('You really like Apple!');
+}
+if (favorite_fruits.includes('Mango')) {
+    console.log('You really like Mango!');
+}
+if (favorite_fruits.includes('Orange')) {
+    console.log('You really like Orange!');
+}
+if (favorite_fruits.includes('Banana')) {
+    console.log('You really like Banana!');
+}
+if (favorite_fruits.includes('Grapes')) {
+    console.log('You really like Grapes!');
+}
