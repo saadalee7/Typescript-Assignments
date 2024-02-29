bded8c70eb20e98a3fff981f0fb704e63549bbd5
Muhammad Saad <160862157+saadalee7@users.noreply.github.com>
2024-03-01 00:29:04 +0500

    Add files via upload

---
+++ 27-Alien-Colors-#3/main.js
@@ -0,0 +1,38 @@
+"use strict";
+// Version 1 : Using if-else chain
+let alien_color = 'yellow';
+if (alien_color === 'green') {
+    console.log('Congratulations! You just earned 5 points.');
+}
+else if (alien_color === 'red') {
+    console.log('Congratulations! You just earned 10 points.');
+}
+else if (alien_color === 'yellow') {
+    console.log('Congratulations! You just earned 15 points.');
+}
+else {
+    console.log('Invalid alien color!');
+}
+// Version 2 : Using if-else chain with function
+function calculatePoint(alien_color) {
+    if (alien_color === 'green') {
+        console.log('Congratulations! You just earned 5 points.');
+    }
+    else if (alien_color === 'yellow') {
+        console.log('Congratulations! You just earned 10 points.');
+    }
+    else if (alien_color === 'red') {
+        console.log('Congratulations! You just earned 15 points.');
+    }
+    else {
+        console.log('Invalid alien color!');
+    }
+}
+calculatePoint('yellow');
+// Version 3 : Using a ternary operator
+alien_color = 'yellow';
+const messagePoint = alien_color === 'yellow' ? 'Congratulations! You just earned 5 points.' :
+    alien_color === 'red' ? 'Congratulations! You just earned 10 points.' :
+        alien_color === 'green' ? 'Congratulations! You just earned 15 points.' :
+            'Invalid alien color';
+console.log(messagePoint);
