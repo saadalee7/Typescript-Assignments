2f6b01e0732e877910ebdaa05a536858174a262f
Muhammad Saad <160862157+saadalee7@users.noreply.github.com>
2024-03-20 23:26:47 +0500

    Add files via upload

---
+++ 13-Your-Own-Array/app.js
@@ -0,0 +1,6 @@
+"use strict";
+let transportation = ["Motercycle", "Car", "Train", "Cycle"];
+let i = 0;
+for (let i = 0; i < transportation.length; i++) {
+    console.log("I would like to own a " + transportation[i]);
+}
---
+++ 13-Your-Own-Array/app.ts
@@ -0,0 +1,5 @@
+let transportation :string[] = ["Motercycle", "Car", "Train", "Cycle"];
+let i = 0;
+for(let i=0; i<transportation.length; i++){
+    console.log("I would like to own a " + transportation[i]);
+}
